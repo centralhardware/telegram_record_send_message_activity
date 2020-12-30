21a7f55ab0347f92a3670109f952d3f75739c0f8
Fedechkin Alexey <alex@centralhardware.ru>
2020-12-30 15:13:33 +0700

    user chat.title instead of username

--- src/main.py
+++ src/main.py
@@ -10,32 +10,32 @@ api_hash = os.getenv('API_HASH')
 telephone = os.getenv('TELEPHONE')
 client = TelegramClient('alex', api_id, api_hash)
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-port = os.getenv("DB_PORT")
-database_name = os.getenv("DATABASE_NAME")
-connection = psycopg2.connect(user=user,
-                              password=password,
-                              host=host,
-                              port=port,
-                              database=database_name)
-cursor = connection.cursor()
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS statistic (
-        id serial PRIMARY KEY,
-        message TEXT,
-        chat TEXT,
-        created_at TIMESTAMP NOT NULL
-    );
-""")
-connection.commit()
+# user = os.getenv("DB_USER")
+# password = os.getenv("DB_PASSWORD")
+# host = os.getenv("DB_HOST")
+# port = os.getenv("DB_PORT")
+# database_name = os.getenv("DATABASE_NAME")
+# connection = psycopg2.connect(user=user,
+#                               password=password,
+#                               host=host,
+#                               port=port,
+#                               database=database_name)
+# cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS statistic (
+#         id serial PRIMARY KEY,
+#         message TEXT,
+#         chat TEXT,
+#         created_at TIMESTAMP NOT NULL
+#     );
+# """)
+# connection.commit()
 
 
 @client.on(events.NewMessage(outgoing=True))
 async def handler(event):
     if event.raw_text != '/ping':
-        logging.info("triggered in chat %s\n on message: %s\n", event.chat.username, event.raw_text)
+        logging.info("triggered in chat %s\n on message: %s\n", event.chat.title, event.raw_text)
         cursor.execute("""
             INSERT INTO statistic ( 
                 message,
@@ -43,7 +43,7 @@ async def handler(event):
                 created_at
             ) 
             VALUES ( '{}', '{}', current_timestamp );""".format(event.raw_text,
-                                                                event.chat.username))
+                                                                event.chat.title))
         connection.commit()
 
 
