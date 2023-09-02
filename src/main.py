ba51954716272daa0af5a210b1e74b11852fdcb3
Alexey Fedechkin <alex@centralhardware.me>
2023-09-02 18:17:06 +0200

    fix code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-import datetime
+from datetime import datetime
 import logging
 import os
 
 import clickhouse_connect
 from telethon import events, TelegramClient
+
+logging.basicConfig(level=logging.INFO)
 
 api_id = int(os.getenv('API_ID'))
 api_hash = os.getenv('API_HASH')
@@ -13,8 +15,8 @@ client = TelegramClient('alex', api_id, api_hash)
 user = os.getenv("DB_USER")
 password = os.getenv("DB_PASSWORD")
 host = os.getenv("DB_HOST")
-client = clickhouse_connect.get_client(host=host, port=8123, username=user, password=password)
-client.command("""
+clickhouse = clickhouse_connect.get_client(host=host, port=8123, username=user, password=password)
+clickhouse.command("""
     CREATE TABLE IF NOT EXISTS telegram_messages (
                                                  date_time DateTime,
                                                  message String,
@@ -24,20 +26,19 @@ client.command("""
 
 @client.on(events.NewMessage(outgoing=True))
 async def handler(event):
-    if event.raw_text != '/ping' and event.chat is not None:
-        if hasattr(event.chat, "title"):
-            title = event.chat.title
-        elif event.chat.username is not None:
-            title = event.chat.username
-        else:
-            return
-        if event.raw_text != '':
-            logging.info("triggered in chat %s\n on message: %s\n", title, event.raw_text)
-            data = [datetime.datetime, event.raw_text, title]
-            client.insert('telegram_messages',data, column_names=['date_time', 'messages', 'chat'])
-            logging.info("insert new message")
-        else:
-            logging.info("ignore empty message")
+    if hasattr(event.chat, "title"):
+        title = event.chat.title
+    elif event.chat.username is not None:
+        title = event.chat.username
+    else:
+        return
+    if event.raw_text != '':
+        logging.info("triggered in chat %s\n on message: %s\n", title, event.raw_text)
+        data = [[datetime.now(), event.raw_text, title],]
+        clickhouse.insert('telegram_messages', data, ['date_time', 'message', 'chat'])
+        logging.info("insert new message")
+    else:
+        logging.info("ignore empty message")
 
 client.connect()
 client.start(phone=telephone)
